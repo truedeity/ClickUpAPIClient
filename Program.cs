b3626dcd5840de4e9647d5ea4ab851159115d26c
Daniel Newman <dan.newman1982@gmail.com>
2023-02-03 01:14:36 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ClickUpAPIClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("Authorization", "Token YOUR_API_KEY");
+                var response = await client.GetAsync("https://api.clickup.com/api/v2/task");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var tasks = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine(tasks);
+                }
+                else
+                {
+                    Console.WriteLine("Request failed with status code: " + response.StatusCode);
+                }
+            }
+        }
+    }
+}
